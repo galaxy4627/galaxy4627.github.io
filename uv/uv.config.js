e87aa0704c61b3f8b4d863edf2af1f48f4c56a1e
desplmfao <98325692+desplmfao@users.noreply.github.com>
2022-03-21 20:13:29 -0400

    Add files via upload

---
+++ uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/?p=',
+    bare: 'https://uv1.aeroisthebestproxy.cf',
+    encodeUrl: Ultraviolet.codec.plain.encode,
+    decodeUrl: Ultraviolet.codec.plain.decode,
+    handler: '/uv.handler.js',
+    bundle: '/uv.bundle.js',
+    config: '/uv.config.js',
+    sw: '/uv.sw.js',
+};
