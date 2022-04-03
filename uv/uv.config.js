439cef39c62ab88f36411302f87216ca96ead119
desplmfao <98325692+desplmfao@users.noreply.github.com>
2022-04-03 18:52:02 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/?p=',
-    bare: 'https://uv2.aeroisthebestproxy.cf',
+    bare: 'https://lol.aeroisass.com',
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
     handler: '/uv/uv.handler.js',
