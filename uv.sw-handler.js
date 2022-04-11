fd88927f16b70e5f71b45be76685699637424824
desplmfao <98325692+desplmfao@users.noreply.github.com>
2022-04-10 20:04:29 -0400

    Update uv.sw-handler.js

--- uv.sw-handler.js
+++ uv.sw-handler.js
@@ -6,9 +6,6 @@ sw.on('request', event => {
 
 })
 
-if (url == "www.pornhub.com") url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-if (url == "pornhub.com") url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-
 self.addEventListener('fetch', event => {
     event.respondWith(
         sw.fetch(event)
