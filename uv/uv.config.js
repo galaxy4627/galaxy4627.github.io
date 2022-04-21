c8508a2a3fee6edcb95a571e4afca41b785fb5da
galaxy4627 <100154768+galaxy4627@users.noreply.github.com>
2022-04-21 16:19:57 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://incog.dev/bare/',
+    bare: 'https://bare.mathstudies.tk/bin/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
