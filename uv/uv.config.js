d7622ccaaa0fd6b4c933bb7038c06b76809c6b54
galaxy4627 <100154768+galaxy4627@users.noreply.github.com>
2022-04-22 17:06:53 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://mathaway.ml/',
+    bare: 'https://client.bareserver.tk/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
