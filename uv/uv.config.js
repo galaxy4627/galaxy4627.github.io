8969d8c4c06691a1a4e235d962d13144c414e36d
galaxy4627 <100154768+galaxy4627@users.noreply.github.com>
2022-04-27 12:29:09 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://c.googletranslate.gq',
+    bare: 'https://c.googletranslate.gq/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
