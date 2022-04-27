685a977eee03e236a7dfcc012773bdeaf67eb17f
galaxy4627 <100154768+galaxy4627@users.noreply.github.com>
2022-04-27 10:31:26 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://bare.aeroisass.com/',
+    bare: 'https://client.bareserver.tk/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
