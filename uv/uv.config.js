cb4ebb974fda59db5f83cabd53119e517e007cfb
galaxy4627 <100154768+galaxy4627@users.noreply.github.com>
2022-05-04 13:50:09 -0700

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,9 @@
+const split = location.hostname.split('.');
+const domain = split.slice(0).slice(-(split.length === 4 ? 3 : 2)).join('.')
+
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://c.googletranslate.gq/',
+    bare: 'https://client.'+domain+'/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
